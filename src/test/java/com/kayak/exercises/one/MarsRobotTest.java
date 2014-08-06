e983287975fa6195ad5a1a6e5a45d0e5f0a92f1b
Anton Smolich <a.smolich@gmail.com>
2014-08-07 01:47:07 +0200

    Negative tests added

--- src/test/java/com/kayak/exercises/one/MarsRobotTest.java
+++ src/test/java/com/kayak/exercises/one/MarsRobotTest.java
@@ -5,7 +5,9 @@ import static org.junit.Assert.assertEquals;
 import java.util.Arrays;
 import java.util.Collection;
 
+import org.junit.Rule;
 import org.junit.Test;
+import org.junit.rules.ExpectedException;
 import org.junit.runner.RunWith;
 import org.junit.runners.Parameterized;
 import org.junit.runners.Parameterized.Parameters;
@@ -17,10 +19,18 @@ public class MarsRobotTest {
     private Point endPoint;
     private Direction endDirection;
 
-    public MarsRobotTest(final String command, final Point endPoint, final Direction endDirection) {
+    @Rule
+    public ExpectedException expectedException = ExpectedException.none();
+
+    public MarsRobotTest(final String command, final Point endPoint, final Direction endDirection, final Class<? extends Throwable> exception) {
         this.command = command;
         this.endPoint = endPoint;
         this.endDirection = endDirection;
+        if (exception == null) {
+            expectedException = ExpectedException.none();
+        } else {
+            expectedException.expect(exception);
+        }
     }
 
     @Test
@@ -48,17 +58,21 @@ public class MarsRobotTest {
     @Parameters(name="{index}: expected {1} {2} for command {0}")
     public static Collection<Object[]> data() {
         return Arrays.asList(new Object[][] {
-                { "LLLLLLLLLLLL", new Point(0,0), Direction.NORTH },
-                { "RRRRRRRRRRRR", new Point(0,0), Direction.NORTH },
-                { "LR", new Point(0,0), Direction.NORTH },
-                { "LLRR", new Point(0,0), Direction.NORTH },
-                { "FF", new Point(0,2), Direction.NORTH },
-                { "FFRFFRFFRFF", new Point(0,0), Direction.WEST },
-                { "FFFLLFFF", new Point(0,0), Direction.SOUTH },
-                { "FFRFFLFL", new Point(2,3), Direction.WEST },
-                { "LFFLF", new Point(-2,-1), Direction.SOUTH },
-                { "LFFFRFFFRRFFF", new Point(-3, 0), Direction.SOUTH },
-                { "LFFFL", new Point(-3, 0), Direction.SOUTH }
+                { "LLLLLLLLLLLL", new Point(0,0), Direction.NORTH, null },
+                { "RRRRRRRRRRRR", new Point(0,0), Direction.NORTH, null },
+                { "LR", new Point(0,0), Direction.NORTH, null },
+                { "LLRR", new Point(0,0), Direction.NORTH, null },
+                { "FF", new Point(0,2), Direction.NORTH, null },
+                { "FFRFFRFFRFF", new Point(0,0), Direction.WEST, null },
+                { "FFFLLFFF", new Point(0,0), Direction.SOUTH, null },
+                { "FFRFFLFL", new Point(2,3), Direction.WEST, null },
+                { "LFFLF", new Point(-2,-1), Direction.SOUTH, null },
+                { "LFFFRFFFRRFFF", new Point(-3, 0), Direction.SOUTH , null},
+                { "LFFFL", new Point(-3, 0), Direction.SOUTH , null},
+
+                { "", new Point(0, 0), Direction.NORTH, null },
+                { null, new Point(0, 0), Direction.NORTH, IllegalArgumentException.class },
+                { "INVALID_COMMAND", new Point(0, 0), Direction.NORTH, UnsupportedOperationException.class }
         });
     }
 }
