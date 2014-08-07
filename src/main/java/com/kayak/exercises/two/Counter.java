f1aca62f07706979ab64f1f7222730e967725726
Anton Smolich <a.smolich@gmail.com>
2014-08-07 07:55:34 +0200

    Comments added

--- src/main/java/com/kayak/exercises/two/Counter.java
+++ src/main/java/com/kayak/exercises/two/Counter.java
@@ -1,23 +1,47 @@
 package com.kayak.exercises.two;
 
+/**
+ * Class has methods to count in sequential order without using any iterative programming loops,
+ * i.e. while, for, do, for-each, etc
+ *
+ * @author asmolich
+ */
 public class Counter {
 
-    public void countUp(final int start, final int end) {
+    /**
+     * Prints out numbers in sequential order from start to end inclusive. Each number on new line.
+     *
+     * @param start. start must be > 0
+     * @param end. end must be > 0 and > start
+     */
+    public static void countUp(final int start, final int end) {
         checkPreconditions(start, end);
         countUp0(start, end);
     }
 
-    public void countDown(final int start, final int end) {
+    /**
+     * Prints out numbers in sequential order from end to start inclusive. Each number on new line.
+     *
+     * @param start. start must be > 0
+     * @param end. end must be > 0 and > start
+     */
+    public static void countDown(final int start, final int end) {
         checkPreconditions(start, end);
         countDown0(start, end);
     }
 
-    public void countUpAndDown(final int start, final int end) {
+    /**
+     * Prints out numbers in sequential order from start to end (only once), but then also counts down to the start
+
+     * @param start. start must be > 0
+     * @param end. end must be > 0 and > start
+     */
+    public static void countUpAndDown(final int start, final int end) {
         checkPreconditions(start, end);
         countUpAndDown0(start, end);
     }
 
-    private void checkPreconditions(final int start, final int end) {
+    private static void checkPreconditions(final int start, final int end) {
         if (start < 0 || end < 0) {
             throw new IllegalArgumentException("Start and end must be non-negative");
         }
@@ -26,7 +50,7 @@ public class Counter {
         }
     }
 
-    private void countUp0(final int start, final int end) {
+    private static void countUp0(final int start, final int end) {
         if (end <= start) {
             System.out.println(start);
         } else {
@@ -35,7 +59,7 @@ public class Counter {
         }
     }
 
-    private void countDown0(final int start, final int end) {
+    private static void countDown0(final int start, final int end) {
         if (end <= start) {
             System.out.println(end);
         } else {
@@ -44,13 +68,13 @@ public class Counter {
         }
     }
 
-    private void countUpAndDown0(final int start, final int end) {
+    private static void countUpAndDown0(final int start, final int end) {
         countUp0(start, end - 1);
         System.out.println(end);
         countDown0(start, end - 1);
     }
 
-    //private void countUpAndDown2(final int start, final int end) {
+    //private static void countUpAndDown2(final int start, final int end) {
     //    if (end == start) {
     //        System.out.println(start);
     //        return;
@@ -61,12 +85,12 @@ public class Counter {
     //}
 
     public static void main(final String[] args) {
-        // new Counter().countUp(0,5);
-        new Counter().countUpAndDown(0, 5);
-        // new Counter().countUp(0,1000000000);
-        // new Counter().countUpAndDown(0,1000000000);
-        // new Counter().countUpAndDown(0,Integer.MAX_VALUE);
-        // new Counter().countUpAndDown(10, 15);
-        // new Counter().countUpAndDown2(0,1000000000);
+        // Counter.countUp(0,5);
+        Counter.countUpAndDown(0, 5);
+        // Counter.countUp(0,1000000000);
+        // Counter.countUpAndDown(0,1000000000);
+        // Counter.countUpAndDown(0,Integer.MAX_VALUE);
+        // Counter.countUpAndDown(10, 15);
+        // Counter.countUpAndDown2(0,1000000000);
     }
 }
